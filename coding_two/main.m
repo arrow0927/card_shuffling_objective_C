071f5952fc8f98220774c19d0232e1a06c8e9735
AM <amulekar@gmail.com>
2014-05-09 19:44:39 -0400

    Initial Commit

---
+++ coding_two/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  coding_two
+//
+//  Created by AM on 2014-05-09.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
